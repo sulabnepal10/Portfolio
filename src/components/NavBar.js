3f67055db0b622af3f61fef519a78aaf7a73a020
sulabnepal100 <sulab@clarify.ai>
2026-06-13 22:59:35 +0545

    Updated the new project link to the Project section

---
+++ src/components/NavBar.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from '../assets/img/logo-boat.png';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+import { HashLink } from 'react-router-hash-link';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+
+export const NavBar = () => {
+
+  const [activeLink, setActiveLink] = useState('home');
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [])
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  }
+
+  return (
+    <Router>
+      <Navbar expand="md" className={scrolled ? "scrolled" : ""} style={{ zIndex: 1000 }}>
+        <Container>
+
+          <Navbar.Collapse id="basic-navbar-nav">
+
+            <span className="navbar-text">
+              <div className="social-icon">
+                <a href="https://www.linkedin.com/in/sulab-nepal-470ab0239" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.instagram.com/nepalsulab/" target="_blank" rel="noopener noreferrer"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.facebook.com/sulab.nepal.9?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer"><img src={navIcon2} alt="" /></a>
+              </div>
+            </span>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Router>
+  )
+}
